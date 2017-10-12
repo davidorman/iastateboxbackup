b1fee885cf15af52d4ddfe1034fb8a3d8a34b6a8
snehring <snehring@iastate.edu>
2017-10-12 17:49:12 -0500

    fleshing out backup a bit refactoring

--- boxbackup.py
+++ boxbackup.py
@@ -5,6 +5,7 @@ import hashlib
 from Crypto.Cipher import AES
 import os
 
+
 class AESDecrypt(object):
     def __init__(self, key):
         self.key = hashlib.sha256(key.encode()).digest()
@@ -21,14 +22,17 @@ class AESDecrypt(object):
         # decode as utf-8
         return data.decode('utf-8')
 
+
 class BoxBackupException(Exception):
     """
     I'll make my own damn exception class
     """
     def __init__(self, message):
         self.message = message
+
     def __unicode__(self):
         return "Exception: " + self.message
+
 
 # Since I am lazy and there's not a great way to handle OAuth bs
 # from the command line, we make the user do some of the steps
@@ -40,29 +44,33 @@ def get_authenticated_client():
     CLIENT_ID = None
     CLIENT_SECRET = None
     ACCESS_TOKEN = None
-    # client and secret are in a config file 
+    # client and secret are in a config file
     with open('auth.cfg', 'r') as auth_cfg:
         # 100% guaranteed unhackable
         aes = AESDecrypt(base64.b64decode('b2JmdXNjYXRpb24h').decode('utf-8'))
         CLIENT_ID = aes.decrypt(auth_cfg.readline().strip())
         CLIENT_SECRET = aes.decrypt(auth_cfg.readline().strip())
     oauth = OAuth2(CLIENT_ID, CLIENT_SECRET)
-    auth_url, csrf_token = oauth.get_authorization_url("https://iastateboxbackup")
-    
+    auth_url, csrf_token =
+    oauth.get_authorization_url("https://iastateboxbackup")
     # ask user to go to the url and authorize our app
-    print("Please go to "+auth_url+" in your web browser.\nYou will be asked to"
-            +" authorize this app. Once you have done so you will be redirected to"
-            +" an invalid webpage. Go to the address bar in your browser and copy"
-            +" the string of random letters and numbers after 'code=' this is your"
-            +" access token. You will need to paste this token here within 30 seconds.")
+    print("Please go to "+auth_url+" in your web browser.\n\n" +
+          " You will be asked to authorize this app." +
+          " Once you have done so you will be redirected to an invalid" +
+          " webpage. Go to the address bar in your browser and copy" +
+          " the string of random letters and numbers after 'code='" +
+          " this is your access token. You will need to paste this" +
+          " token here within 30 seconds.")
     ACCESS_TOKEN = input("Please paste your acccess token here:")
     access_token, refresh_token = oauth.authenticate(ACCESS_TOKEN)
     client = Client(oauth)
     return client
 
+
 def logout_client(client):
     """Should invalidate our session. To be called when done."""
     client.auth.revoke()
+
 
 def recurse_backup(box_folder, folder, follow_links=False):
     """Takes in the current working box_folder and the current fs folder
@@ -72,11 +80,59 @@ def recurse_backup(box_folder, folder, follow_links=False):
     # os.walk could be useful here too but I think it might be easier
     # to keep track of state recursively
     elements = os.listdir(folder)
+    if not e:
+        # probably a good enough basecase
+        return
+
     for e in elements:
-        if os.path.isdir(os.path.join(folder,e)):
+        element_path = os.path.join(folder, e)
+        if os.path.isdir(element_path):
             # directory: create box folder and recurse
-        elif os.path.isfile(os.path.join(folder,e)):
+            new_folder = box_folder.create_subfolder(os.path.basename(e))
+            recurse_backup(new_folder, element_path)
+        elif os.path.isfile(element_path):
             # file: backup
+            box_folder.upload(element_path, file_name=e,
+                              preflight_check=True,
+                              preflight_expected_size=os.stat(element_path))
+
+
+def get_backup_root(folder_name):
+    """get a box folder that is to be the backup_root, create if not found"""
+    backup_root = None
+    # check if folder_name exists
+    search _results = client.search(query=folder_name, limit=100, offset=1,
+                                    result_type='folder')
+    if not search_results:
+        # no results does not necessarily mean the folder doesn't exist
+        try:
+            backup_root = root.create_subfolder(name=folder_name)
+        except BoxAPIException as e:
+            if e.code == "item_name_in_use":
+                # name collision. Assume we want to use it.
+                # should I even bother with the search if
+                # this is the behavior I'm going with? end result same
+                # decide and refactor if necessary
+                backup_root = client.folder(folder_id=e.context_info['conflicts'][0]['id']).get()
+            else:
+                print("Caught an unexpected exception while creating" +
+                      " backup_root")
+                raise
+    else:
+        # search had some hits
+        for i in search_results:
+            if i.name == folder_name:
+                backup_root = i
+                break
+            else:
+                continue
+        # should have folder now check though
+    if not backup_root:
+        raise BoxBackupException("Failed to get a backup_root")
+    else:
+        return backup_root
+
+
 def main():
     # TODO
     client = get_authenticated_client()
@@ -84,34 +140,7 @@ def main():
         print("Username: "+client.user(user_id='me').get()['login'])
         # for now let's assume that we always backup to the same folder and just create a subfolder within it that is this backup
         root = client.folder(folder_id='0')
-        backup_root = None
-        # check if a folder called 'iastateboxbackup' exists yet
-        # I have no idea in what order things are returned, but let's get many in case
-        search_results = client.search(query="iastateboxbackup", limit=100,offset=0,result_type='folder')
-        if not search_results:
-            #no results create folder sometimes folders take a while to show up so we may have to deal with that
-            try:
-                backup_root = root.create_subfolder(name="iastateboxbackup")
-            except BoxAPIException as e:
-                if e.code == "item_name_in_use":
-                    #name collision. Assume we can use it
-                    backup_root = client.folder(folder_id=e.context_info['conflicts'][0]['id']).get()
-                else:
-                    print("Caught an exception we didn't expect.")
-                    raise
-        else:
-            #folder found get id
-            for i in search_results:
-                    if i.name == "iastateboxbackup":
-                        backup_root = i
-                        break
-                    else:
-                        continue
-        # should have folder now
-        # how about we actually check that
-        if not backup_root:
-            raise BoxBackupException("Failed to get a backup directory.")
-
+        backup_root = get_backup_root("iastateboxbackup")
         # either get directory we're backing up or assume it's cwd TODO decide which or both
         # interactive or specified at invocation time?
 
@@ -123,7 +152,6 @@ def main():
         # Also need logic to check file size against limit and log error and skip file if too large
 
         # Done backing up
-
 
     except BoxException as e:
         print("Caught a Box exception: "+e)
